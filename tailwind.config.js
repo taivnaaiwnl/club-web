dec2a7304ef5c7c414bac14faf5bfa35251fef79
taivnaaiwnl <taivnaaiwnl@gmail.com>
2024-09-01 12:02:37 +0800

    club web

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'new-amsterdam': ['"New Amsterdam"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
